1c7c2a4115ec13ace4c68446fc0822176a8a1676
STEPHEN AGER <steve_ager@hotmail.com>
2023-01-13 11:01:41 -0700

    added method

--- HelloWorld.Solution/HelloWorld.Console/MessageProvider.cs
+++ HelloWorld.Solution/HelloWorld.Console/MessageProvider.cs
@@ -3,11 +3,15 @@
     public interface IMessageProvider
     {
         string GetMessage();
+
+        string GetMessage(string who);
     }
 
 
     public class MessageProvider : IMessageProvider
     {
         public string GetMessage() => "Hello world!";
+
+        public string GetMessage(string who) => $"Hello {who}!";
     }
 }
